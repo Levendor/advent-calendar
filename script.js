2d007c241c95478b7bd7ef9117bcfec8776c2d56
levendor <myth_of_lavanda@mail.ru>
2024-11-30 02:50:45 +0100

    feat: add inline input and textes

--- script.js
+++ script.js
@@ -1,38 +1,271 @@
 const content = [
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
-  { riddle: 'riddle<br>riddle<br>riddle<br>riddle', answer: 'answer', prize: 'prize<br>prize<br>prize<br>prize' },
+  { 
+    riddle: `Пёстрый хоровод ребят<br>
+Через каждый год летят<br>
+Кто из них верхом на вьюге<br>
+Замыкает этот ряд?`,
+    answer: `декабрь`,
+    prize: `Да! И пусть он будет снежный<br>
+И не злой, и очень нежный<br>
+Пусть несёт благие вести<br>
+А не то что этот, прежний`,
+  },
+  { 
+    riddle: `Можно сделать на удачу<br>
+Мой шарообразный пуз<br>
+Это я весною плачу<br>
+А зимой всегда смеюсь`,
+    answer: `снеговик`,
+    prize: `Вместо носика - морковка<br>
+Пара веток вместо рук<br>
+Скатан я из снега ловко<br>
+Обнимашками делюсь!`,
+  },
+  { 
+    riddle: `Я зимой стою в лесу, издали видна<br>
+Шубою пушистою вся окружена<br>
+Если хочешь праздничный подарить настрой<br>
+Будь со мною вежлив и забери домой`,
+    answer: `елка`,
+    prize: `Яркую гирлянду на меня надень<br>
+Блеск игрушек праздничных наполнит этот день<br>
+Пусть возьмётся за руки, песню запоёт<br>
+Пусть идёт вокруг меня, кружит хоровод!`,
+  },
+  { 
+    riddle: `Бубенчики звенят, несутся лесом кони<br>
+Они спешат к тебе доставить очень важный груз<br>
+Как только выйду из саней - праздник к нам приходит<br>
+А ну, скажите дети - как меня зовут?`,
+    answer: `дед мороз`,
+    prize: `Я принёс вам волшебство! Это знает всякий -<br>
+Новый год в себе таит красок карнавал<br>
+Дед Мороз приносит детям радость и подарки<br>
+Но лишь тем, кто добрым был и маме помогал`,
+  },
+  { 
+    riddle: `Я белый, я пушистый, я бываю<br>
+Во всех углах и куче разных мест<br>
+Я землю покрывалом покрываю<br>
+Почти всегда! Но только летом нет`,
+    answer: `снег`,
+    prize: `Я белая перина! Я мягкая постель!<br>
+Я горная лавина! Я лёгкий снегопад!<br>
+Хрустящие следы - ноты моей песни<br>
+И искры от луны на мне горят.`,
+  },
+  { 
+    riddle: `Я был рождён идеей, что как искра сияла<br>
+Я был сокрытый тайной и замыслом храним<br>
+Ты под покровом ночи мой образ собирала<br>
+Чтобы в один прекрасный миг ему меня вручить`,
+    answer: `подарок`,
+    prize: `Пусть только блеск в его глазах моею был наградой<br>
+Ведь всё равно, каким я был, и во что одет<br>
+Поскольку главное - любовь, с которой создавался<br>
+Любовь, которую дашь в дар и обретёшь в ответ`,
+  },
+  { 
+    riddle: `Вино налито по бокалам, нарезаны салаты<br>
+Скоро будет бой часов, накрытый стол зовёт<br>
+Все мы верим - эта ночь на чудеса богата<br>
+Все мечты исполнит праздник input`,
+    answer: `новый год`,
+    prize: `На двенадцатый удар тосты зазвучали<br>
+Пепел заклинаний поглощён в устах<br>
+Каждое желание звёзды записали<br>
+На бумаге нового чистого листа`,
+  },
+  { 
+    riddle: `Этот звук предвестником выступает чуда<br>
+Вызывает этот звук радость у ребят<br>
+Празднует честной народ, бахает салюты<br>
+Напевает "input радостно звенят!"`,
+    answer: `бубенцы`,
+    prize: `Бубенцы, бубенцы радостно звенят!<br>
+Звон идёт во все концы, саночки летят!<br>
+Веселись! Запевай! Встань-ка в хоровод!<br>
+Праздник к нам приходит, праздник новый год!`,
+  },
+  { 
+    riddle: `У всех бывает отпуск, сладкая пора<br>
+Можно не работать и даже отдохнуть<br>
+Горестно вздыхает в школе детвора<br>
+Перерыв в учёбе есть когда-нибудь?`,
+    answer: `каникулы`,
+    prize: `Позабыты хлопоты, выкинут рюкзак<br>
+Позади дивана пылится пиджак<br>
+Средь игрушек высится цитадель из книг<br>
+Спит, блаженно щурится, утром ученик...`,
+  },
+  { 
+    riddle: `Древние китайцы что-то понимали<br>
+Каждый год и цветом, и животным нарекли<br>
+Будет двадцать пятый годом деревянной<br>
+И как листва на дереве зелёной input`,
+    answer: `змеи`,
+    prize: `Много не сказать о змее хорошего<br>
+Говорят, рептилии стали всех умней<br>
+Может быть, мудрее змеи были в прошлом<br>
+Новый год пусть мудростью осенит людей`,
+  },
+  { 
+    riddle: `Этот крендель как будто из Франции<br>
+Гордо стоит на столе, как месье.<br>
+Был бы человеком - удивил бы танцами<br>
+Ну а так порадует вкусом input`,
+    answer: `оливье`,
+    prize: `Выпивай да ешь, не забывай про песни<br>
+Новогодний стол ломится от блюд<br>
+Кто здесь блюдо главное - всем давно известно<br>
+Надо теперь выяснить, что вкуснее пьют!`,
+  },
+  { 
+    riddle: `Сегодня задание будет сложнее<br>
+Но ты напрягись, и тогда волшебство<br>
+Явится миру. Готова? Проверим!<br>
+Как по-английски сказать "рождество"?`,
+    answer: `christmas`,
+    prize: `Бинго! Тобою маэстро доволен<br>
+Запросто взяла овраги и рвы<br>
+Видно, не зря на своём телефоне<br>
+Брала задания зелёной совы`,
+  },
+  { 
+    riddle: `Чем бы заняться вечером зимним?<br>
+Чай надоел, не хочу пирожки<br>
+Может покинем стены квартиры<br>
+Выйдем во двор, поиграем в input`,
+    answer: `снежки`,
+    prize: `Хватаем детей и бегом одеваться<br>
+Шапку надень, пальтецо натяни<br>
+Лепим снежки неодетыми пальцами<br>
+Но страшно не это - лицо береги!`,
+  },
+  { 
+    riddle: `Итак, мы решили украсить квартиру<br>
+Игрушек на ёлку повесили сто<br>
+Снежинки на окна, на стены, на двери...<br>
+Так, кажется, что-то забыли... Но что?`,
+    answer: `гирлянда`,
+    prize: `Теперь коридор освещён синим светом<br>
+И звёзды на окнах будут сиять<br>
+Красивая ёлка мигает приветливо<br>
+Найти бы желание позже всё снять`,
+  },
+  { 
+    riddle: `riddle<br>riddle<br>riddle<br>riddle`,
+    answer: `answer`,
+    prize: `prize<br>prize<br>prize<br>prize`,
+  },
+  { 
+    riddle: `riddle<br>riddle<br>riddle<br>riddle`,
+    answer: `answer`,
+    prize: `prize<br>prize<br>prize<br>prize`,
+  },
+  { 
+    riddle: `riddle<br>riddle<br>riddle<br>riddle`,
+    answer: `answer`,
+    prize: `prize<br>prize<br>prize<br>prize`,
+  },
+  { 
+    riddle: `riddle<br>riddle<br>riddle<br>riddle`,
+    answer: `answer`,
+    prize: `prize<br>prize<br>prize<br>prize`,
+  },
+  { 
+    riddle: `riddle<br>riddle<br>riddle<br>riddle`,
+    answer: `answer`,
+    prize: `prize<br>prize<br>prize<br>prize`,
+  },
+  { 
+    riddle: `riddle<br>riddle<br>riddle<br>riddle`,
+    answer: `answer`,
+    prize: `prize<br>prize<br>prize<br>prize`,
+  },
+  { 
+    riddle: `riddle<br>riddle<br>riddle<br>riddle`,
+    answer: `answer`,
+    prize: `prize<br>prize<br>prize<br>prize`,
+  },
+  { 
+    riddle: `riddle<br>riddle<br>riddle<br>riddle`,
+    answer: `answer`,
+    prize: `prize<br>prize<br>prize<br>prize`,
+  },
+  { 
+    riddle: `riddle<br>riddle<br>riddle<br>riddle`,
+    answer: `answer`,
+    prize: `prize<br>prize<br>prize<br>prize`,
+  },
+  { 
+    riddle: `riddle<br>riddle<br>riddle<br>riddle`,
+    answer: `answer`,
+    prize: `prize<br>prize<br>prize<br>prize`,
+  },
+  { 
+    riddle: `riddle<br>riddle<br>riddle<br>riddle`,
+    answer: `answer`,
+    prize: `prize<br>prize<br>prize<br>prize`,
+  },
+  { 
+    riddle: `riddle<br>riddle<br>riddle<br>riddle`,
+    answer: `answer`,
+    prize: `prize<br>prize<br>prize<br>prize`,
+  },
+  { 
+    riddle: `riddle<br>riddle<br>riddle<br>riddle`,
+    answer: `answer`,
+    prize: `prize<br>prize<br>prize<br>prize`,
+  },
+  { 
+    riddle: `riddle<br>riddle<br>riddle<br>riddle`,
+    answer: `answer`,
+    prize: `prize<br>prize<br>prize<br>prize`,
+  },
+  { 
+    riddle: `riddle<br>riddle<br>riddle<br>riddle`,
+    answer: `answer`,
+    prize: `prize<br>prize<br>prize<br>prize`,
+  },
+  { 
+    riddle: `riddle riddle riddle<br>riddle input riddle<br>riddle riddle<br>riddle riddle riddle`,
+    answer: `answer`,
+    prize: `prize<br>prize<br>prize<br>prize`,
+  },
+  { 
+    riddle: `Быстро промчится месяц последний<br>
+Салат прошлогодний, завтрак похмельный<br>
+Вспомнить число подсобит календарь<br>
+Глянь - на дворе наступает input`,
+    answer: `январь`,
+    prize: `Снега за зиму увидишь немало<br>
+Чистый, нетронутый символ начала<br>
+Нового года разлёгся вокруг<br>
+С месяцем новым, любимый мой друг!`,
+  },
 ];
 
-const dayIndex = new Date().getDate() - 1;
+const placeholders = [
+  'неправильно',
+  'неа',
+  'ну нет же',
+  'не так',
+  'серьёзно?',
+  'попробуй ещё',
+  'бред',
+  'бе-бе-бе',
+];
+
+// const dayIndex = new Date().getDate() - 1;
+const dayIndex = Math.floor(Math.random() * content.length);
+console.log(dayIndex);
+
+const body = document.querySelector('body');
+body.style.backgroundImage = `url('img/${Math.ceil(Math.random() * content.length)}.jpg')`;
+body.style.backgroundSize = 'cover';
+body.style.backgroundRepeat = 'no-repeat';
+body.style.backgroundPosition = 'center';
 
 const main = document.getElementById('main');
 
@@ -40,27 +273,43 @@ const riddle = document.createElement('div');
 riddle.classList.add('riddle');
 riddle.innerHTML = content[dayIndex].riddle;
 
-const input = document.createElement('input');
-input.type = 'text';
-input.autofocus = true;
+let input = document.createElement('input');
 input.classList.add('answer');
 
 const prize = document.createElement('div');
 prize.classList.add('prize');
 prize.innerHTML = content[dayIndex].prize;
 
-main.append(riddle, input);
+main.append(riddle);
+const isInnerInput = riddle.innerHTML.includes('input');
+if (isInnerInput) {
+  riddle.innerHTML = riddle.innerHTML.replace('input', input.outerHTML);
+  input = main.querySelector('.answer');
+  input.style.display = 'inline';
+  input.style.width = `${content[dayIndex].answer.length}ch`;
+} else {
+  main.append(input);
+}
+input.autofocus = true;
+input.focus();
 
 input.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
-    if (input.value.toLowerCase() === content[dayIndex].answer) {
-      main.removeChild(input);
+    if (input.value.toLowerCase().replace('ё', 'е') === content[dayIndex].answer) {
+      if (isInnerInput) {
+        riddle.innerHTML = riddle.innerHTML.replace(input.outerHTML, content[dayIndex].answer);
+      } else {
+        main.removeChild(input);
+      }
       main.append(prize);
     } else {
       input.value = '';
-      input.placeholder = 'wrong';
+      const placeholder = placeholders[Math.floor(Math.random() * placeholders.length)];
+      input.style.width = `${placeholder.length}ch`;
+      input.placeholder = placeholder;
       setTimeout(() => {
         input.placeholder = '';
+        input.style.width = `${content[dayIndex].answer.length}ch`;
       }, 1000);
     }
   }
